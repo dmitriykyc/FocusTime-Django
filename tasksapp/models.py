7390ab4d3aeb150e5f0485edb856209416f70eed
Dmitriy_pc_hp <dmitriykyc@gmail.com>
2021-08-27 12:12:05 +0300

    - Создал модель для tasksapp

--- tasksapp/models.py
+++ tasksapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class TasksModel(models.Model):
+    title = models.CharField('Заголовок', max_length=100)
+    description = models.TextField('Описание')
+    is_activ = models.BooleanField('Активность задания', default=True)
+    date_create = models.DateField('Дата создания', auto_now_add=True)
+    date_update = models.DateField('Дата изменения', auto_now=True)
