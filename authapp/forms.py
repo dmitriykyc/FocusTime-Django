c26815177079d22c66d20402a525764c473fa22e
Dmitriy_pc_hp <dmitriykyc@gmail.com>
2021-09-28 13:22:08 +0300

    - Доделал логин и регистрацию - подписал поля - изображение-ссылка на главную

--- authapp/forms.py
+++ authapp/forms.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import User
 from django import forms
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.forms import TextInput, ModelForm, Textarea
 
 from authapp.models import TimeFocusUsers
 
@@ -9,11 +10,16 @@ class UserLoginForm(AuthenticationForm):
         model = TimeFocusUsers
         fields = ('username', 'password')
 
-
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        for field_name, field in self.fields.items():
-            field.widget.attrs['class'] = f'form-control'
+        self.fields['username'].widget.attrs.update({
+            'class': 'form-control',
+            'placeholder': 'Ваш логин'
+        })
+        self.fields['password'].widget.attrs.update({
+            'class': 'form-control',
+            'placeholder': 'Введите пароль'
+        })
 
 
 
@@ -23,11 +29,30 @@ class UserRegistrationForm(UserCreationForm):
         model = TimeFocusUsers
         fields = ('username', 'email')
 
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        for field_name, field in self.fields.items():
-            field.widget.attrs['class'] = f'form-control'
-            field.help_text = ''
-            print(field_name)
-            print("*" * 20)
-            print(field)
+        self.fields['username'].widget.attrs.update({
+            'class': 'form-control',
+            'placeholder': 'Придумайте логин для входа '
+        })
+        self.fields['username'].help_text = ''
+        self.fields['username'].label = 'Ваш логин'
+
+        self.fields['email'].widget.attrs.update({
+            'class': 'form-control',
+            'placeholder': 'Введите E-mail'
+        })
+
+        self.fields['password1'].help_text = ''
+        self.fields['password1'].widget.attrs.update({
+            'class': 'form-control',
+            'placeholder': 'Придумайте пароль'
+        })
+
+        self.fields['password2'].help_text = ''
+        self.fields['password2'].widget.attrs.update({
+            'class': 'form-control',
+            'placeholder': 'Повторите пароль'
+        })
+
--- tasksapp/forms.py
+++ tasksapp/forms.py
@@ -1,26 +1,23 @@
 from django.contrib.auth.models import User
 from django import forms
-
 from tasksapp.models import UserAnswerTasks
+from django.forms import Textarea
+
 
 class UserAnswerForm(forms.ModelForm):
-    # answer = forms.CharField(widget=forms.Textarea(attrs={"class":"form-control"}), label=False)
-    # is_public = forms.BooleanField(label='Сделать ответ публичным')
-    # image = forms.ImageField(widget=forms.ImageField(attrs={"enctype":"multipart/form-data"}))
-    #
     class Meta:
         model = UserAnswerTasks
         fields = ['answer', 'is_public', 'media']
+
 
 
 
 class UserEditForm(forms.ModelForm):
-
     class Meta:
         model = UserAnswerTasks
         fields = ['answer', 'is_public', 'media']
 
-
-
-
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['media'].widget.attrs.update({'class': 'form-control'})
 
