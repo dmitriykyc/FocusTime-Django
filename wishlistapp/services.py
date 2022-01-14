24a29eec3a0c0b91d406dee6e945212b72af7600
kusok <dmitriykyc@gmail.com>
2022-01-14 16:18:56 +0300

    - Покраска в цвет wishlist, можно добавлять и удалять всё в БД - Кнопка со всплывающим окном добать желание

--- wishlistapp/services.py
+++ wishlistapp/services.py
@@ -3,5 +3,8 @@ from wishlistapp.models import WishList
 
 def done_wish(id_wish):
     wish = WishList.objects.get(id=id_wish)
-    wish.is_done = True
+    if wish.is_done:
+        wish.is_done = False
+    else:
+        wish.is_done = True
     wish.save()
